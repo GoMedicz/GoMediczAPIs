e4b241e6df7bd1b99fdeb3da6bfe354c9269a9cf
davidnwogucodes <caprisonepapi@gmail.com>
2023-10-22 04:08:03 -0700

    adjusted error codes

--- controller/support.js
+++ controller/support.js
@@ -14,7 +14,7 @@ const submitMessage = async (req, res) => {
         const { email, message } = req.body;
 
         if (!email || !message) {
-            return res.status(400).json({ error: 'Required fields missing' });
+            return res.send({ statusCode:400, error: 'Required fields missing' });
         }
         const checkUserCode = await User.findOne({ where: { email: email } });
 
@@ -23,9 +23,9 @@ const submitMessage = async (req, res) => {
             message: message,
         });
 
-        return res.status(201).json({ message: 'Message submitted', data: support, senderCode:checkUserCode.user_code });
+        return res.send({statusCode:201, message: 'Message submitted', data: support, senderCode:checkUserCode.user_code });
     } catch (error) {
-        return res.status(500).json({ error: 'Failed to submit Message', message: error.message });
+        return res.send({ statusCode:500, error: 'Failed to submit Message', message: error.message });
     }
 };
 
@@ -34,7 +34,7 @@ const faqsAndContent = async (req, res) => {
         const { title, owner, content } = req.body;
 
         if (!title || !owner, !content) {
-            return res.status(400).json({ error: 'Required fields missing' });
+            return res.send({ statusCode:400, error: 'Required fields missing' });
         }
 
         const faqs = await Faqs.create({
@@ -43,10 +43,10 @@ const faqsAndContent = async (req, res) => {
             content:content
         });
 
-        return res.status(201).json({ message: 'Message submitted', data: faqs });
+        return res.send({statusCode:201, message: 'Message submitted', data: faqs });
     } catch (error) {
         console.log(error)
-        return res.status(500).json({ error: 'Failed to submit Message', message: error.message });
+        return res.send({statusCode:500, error: 'Failed to submit Message', message: error.message });
     }
 };
 
@@ -55,9 +55,9 @@ const getAllMessages = async (req, res) => {
         // Retrieve all submitted messages from the Support model
         const messages = await Support.findAll();
 
-        return res.status(200).json({ data: messages });
+        return res.send({ statusCode:400, data: messages });
     } catch (error) {
-        return res.status(500).json({ error: 'Failed to retrieve messages', message: error.message });
+        return res.send({ statusCode:500, error: 'Failed to retrieve messages', message: error.message });
     }
 };
 
@@ -66,9 +66,9 @@ const getAllFaqsAndContent = async (req, res) => {
         // Retrieve all FAQs and content from the Faqs model
         const faqsAndContent = await Faqs.findAll();
 
-        return res.status(200).json({ data: faqsAndContent });
+        return res.send({ statusCode:200, data: faqsAndContent });
     } catch (error) {
-        return res.status(500).json({ error: 'Failed to retrieve FAQs and content', message: error.message });
+        return res.send({ statusCode:500, error: 'Failed to retrieve FAQs and content', message: error.message });
     }
 };
 
